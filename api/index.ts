39f45638b22f0f114d2abf7479bbd5c595b67ffc
yassin549 <khoualdiyassin26@gmail.com>
2025-09-18 23:11:33 +0100

    commit

---
+++ api/index.ts
@@ -0,0 +1,7 @@
+// api/index.ts
+// This file is the new entry point for Vercel's serverless environment.
+
+import app from '../server/app';
+
+// Vercel expects a default export that is a request handler
+export default app;
